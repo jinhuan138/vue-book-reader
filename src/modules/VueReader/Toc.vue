f6f518fd623c9c2aab3afaba517e30796e495664
lindacong <2796009827@qq.com>
2025-11-18 14:28:32 +0800

    fix: toc

---
+++ src/modules/VueReader/Toc.vue
@@ -0,0 +1,140 @@
+<template>
+  <div v-for="(item, index) in bookToc" :key="index">
+    <button
+      class="tocAreaButton"
+      :class="{ active: item.href === current }"
+      @click="handleClick(item)"
+    >
+      {{ isSubmenu ? ' '.repeat(4) + item.label : item.label }}
+      <div
+        v-if="item.subitems && item.subitems.length > 0"
+        class="expansion"
+        :class="{ open: item.expansion }"
+      ></div>
+    </button>
+    <div
+      v-if="item.subitems && item.subitems.length > 0"
+      v-show="item.expansion"
+    >
+      <!-- 子目录 -->
+      <Toc
+        :toc="item.subitems"
+        :current="current"
+        :setLocation="setLocation"
+        :isSubmenu="true"
+      />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, watchEffect, toRefs, watch } from 'vue'
+export interface TocProps {
+  toc: Array<any>
+  current: string | number
+  setLocation: (href: string | number, close?: boolean) => void
+  isSubmenu?: boolean
+}
+const bookToc = ref<any[]>([])
+const props = withDefaults(defineProps<TocProps>(), {
+  isSubmenu: false,
+})
+const { setLocation } = props
+const { toc, current, isSubmenu } = toRefs(props)
+const handleClick = (item): void => {
+  if (item.subitems && item?.subitems?.length > 0) {
+    item.expansion = !item.expansion
+    console.log('item.href', item.href)
+    setLocation(item.href, false)
+  } else {
+    setLocation(item.href)
+  }
+}
+watch(current, (val) => {
+  console.log('current', current)
+})
+watchEffect(() => {
+  console.log('current', current.value)
+  bookToc.value = toc.value.map((item) => ({
+    ...item,
+    expansion: false,
+  }))
+})
+</script>
+<style scoped>
+/* ↓ */
+.tocAreaButton .expansion::before {
+  transform: rotate(-45deg) translateX(2.5px);
+}
+
+.tocAreaButton .expansion::after {
+  transform: rotate(45deg) translateX(-2.5px);
+}
+
+/* ↑ */
+.tocAreaButton .open::before {
+  transform: rotate(45deg) translateX(2.5px);
+}
+
+.tocAreaButton .open::after {
+  transform: rotate(-45deg) translateX(-2.5px);
+}
+
+.tocAreaButton {
+  user-select: none;
+  appearance: none;
+  background: none;
+  border: none;
+  display: block;
+  font-family: sans-serif;
+  width: 100%;
+  font-size: 0.9em;
+  text-align: left;
+  padding: 0.9em 1em;
+  border-bottom: 1px solid #ddd;
+  color: #aaa;
+  box-sizing: border-box;
+  outline: none;
+  cursor: pointer;
+  position: relative;
+}
+
+.tocAreaButton:hover {
+  background: rgba(0, 0, 0, 0.05);
+}
+
+.tocAreaButton:active {
+  background: rgba(0, 0, 0, 0.1);
+}
+
+.active {
+  color: #1565c0;
+  border-bottom: 2px solid #1565c0;
+}
+
+/* 二级目录 */
+.tocAreaButton .expansion {
+  cursor: pointer;
+  transform: translateY(-50%);
+  top: 50%;
+  right: 12px;
+  position: absolute;
+  width: 10px;
+  background-color: #a2a5b4;
+  transition:
+    transform 0.3s ease-in-out,
+    top 0.3s ease-in-out;
+}
+
+.tocAreaButton .expansion::after,
+.tocAreaButton .expansion::before {
+  content: '';
+  position: absolute;
+  width: 6px;
+  height: 2px;
+  background-color: currentcolor;
+  border-radius: 2px;
+  transition:
+    transform 0.3s ease-in-out,
+    top 0.3s ease-in-out;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import vue from '@vitejs/plugin-vue'
+import { name } from './package.json'
 
 // https://cn.vitejs.dev/config/
 export default defineConfig({
@@ -9,19 +10,18 @@ export default defineConfig({
     copyPublicDir: false,
     emptyOutDir: true,
     sourcemap: false,
-    minify: true,
+    minify: false,
     outDir: 'lib',
     target: ['edge90', 'chrome90', 'firefox90', 'safari15'],
     lib: {
       entry: 'src/modules/index.ts',
-      name: 'vue-book-reader',
-      fileName: (format) => `vue-book-reader.${format}.js`
+      name,
+      fileName: (format) => `${name}.${format}.js`,
+      formats: ['es', 'umd', 'cjs'],
     },
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
       output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue',
         },
