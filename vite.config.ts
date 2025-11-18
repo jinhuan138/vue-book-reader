2cb788a6954406cac139ecda241dca3a93a00aac
lindacong <279*6009827@qq.com>
2025-11-18 14:35:42 +0800

    fix: lib-inject-css

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import vue from '@vitejs/plugin-vue'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import { name } from './package.json'
 
 // https://cn.vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), cssInjectedByJsPlugin()],
+  plugins: [vue(), libInjectCss()],
   build: {
     copyPublicDir: false,
     emptyOutDir: true,
