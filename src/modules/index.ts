049a84ad136ec398356882396b8a03f77a132c02
lindacong <279*6009827@qq.com>
2026-01-05 11:35:24 +0800

    fix: package build

--- src/modules/index.ts
+++
@@ -1,5 +0,0 @@
-import BookView from './BookView/BookView.vue'
-import VueReader from './VueReader/VueReader.vue'
-
-export { BookView, VueReader }
-export default VueReader
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import { resolve } from 'path'
+import { fileURLToPath } from 'url'
 import { name } from './package.json'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://cn.vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +22,7 @@ export default defineConfig({
     outDir: 'lib',
     target: ['edge90', 'chrome90', 'firefox90', 'safari15'],
     lib: {
-      entry: 'src/modules/index.ts',
+      entry: 'src/packages/index.ts',
       name,
       fileName: (format) => `${name}.${format}.js`,
       formats: ['es', 'umd', 'cjs'],
