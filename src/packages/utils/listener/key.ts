6993370804aabed6f7e8e3dd4b28b3783ea6fd40
lindacong <279*6009827@qq.com>
2026-01-05 11:36:34 +0800

    fix: 目录结构

---
+++ src/packages/utils/listener/key.ts
@@ -0,0 +1,27 @@
+/**
+ * Listen for key press
+ * @param {HTMLElement} el - The element to add event listeners to.
+ * @param {function} fn - The listener function.
+ */
+
+type Direction = 'next' | 'prev'
+
+export default function keyListener(
+  el: HTMLElement,
+  fn: (dire: Direction) => void,
+) {
+  el.addEventListener(
+    'keyup',
+    (e: KeyboardEvent) => {
+      // Right or up arrow key indicates next
+      if (e.key === 'ArrowUp' || e.key === 'ArrowRight') {
+        fn('next')
+      }
+      // left or down arrow key indicates next
+      else if (e.key === 'ArrowDown' || e.key === 'ArrowLeft') {
+        fn('prev')
+      }
+    },
+    false,
+  )
+}
